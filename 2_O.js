781eff762add8c6113a654c2143ccb2c70b2f064
nataliia-v <nataly.verbenskaya@gmail.com>
2020-05-19 12:49:33 +0300

    + Open Close Principle

---
+++ 2_O.js
@@ -0,0 +1,58 @@
+// Open Close Principle
+
+class Shape {
+    area() {
+        throw new Error('Area method should be implemented')
+    }
+}
+
+class Square extends Shape {
+    constructor(size) {
+        super();
+        this.size = size;
+    }
+
+    area() {
+        return this.size ** 2
+    }
+}
+
+class Circle extends Shape {
+    constructor(radius) {
+        super();
+        this.radius = radius;
+    }
+    area() {
+        return (this.radius ** 2) * Math.PI
+    }
+}
+
+class Rect extends Shape {
+    constructor(width, height) {
+        super();
+        this.width = width;
+        this.height = height;
+    }
+
+    area() {
+        return this.width * this.height
+    }
+}
+
+
+class AreaCalculator {
+    constructor(shapes=[]) {
+        this.shapes = shapes;
+    }
+
+    sum () {
+        return this.shapes.reduce((acc, shape) => {
+            acc += shape.area();
+            return acc
+            }, 0)
+    }
+}
+
+const calc = new AreaCalculator([new Square(5), new Circle(3), new Rect(4,7)]);
+
+console.log(calc.sum());
